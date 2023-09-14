6667ee9a981390743ec7efa579f0dcbe40db9893
Nesgc <nestorgarciac11@gmail.com>
2023-09-14 02:37:28 -0600

    Managing state

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
@@ -8,8 +11,18 @@ function ListGroup() {
 
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex == index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
